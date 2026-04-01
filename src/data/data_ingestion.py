74cac252840cd6ecc0257c98979b1625c596c8f3
aakashaldankar <haldankar.aakash.01@gmail.com>
2026-04-01 10:23:01 +0530

    added save_data function to data_ingestion.py

--- src/data/data_ingestion.py
+++ src/data/data_ingestion.py
@@ -1,28 +1,48 @@
 from src.logger import get_logger 
 import pandas as pd
+import os
 
 logger=get_logger("src.data_ingestion")
+extracted_data_path='data'
 
 def load_data(url: str):
     try:
         df=pd.read_csv(url)
-        return df
         logger.info("Loaded the dataset from, %s", url)
+        return df
+        
     except Exception as e:
         logger.error('Unexpected error, %s', e)
         raise
 
-def drop_rows_null_price(df: pd.DataFrame):
-    
+def save_data(df: pd.DataFrame, folder: str):
+
     try:
-        row_indices=df.index[(df['price'].isnull())].tolist()
-        df.drop(index=row_indices, inplace=True)
-        logger.info(f"Dropped {len(row_indices)} rows having price null")
-        return df
-    
+        path=os.path.join(extracted_data_path,folder, 'extracted_data.csv')
+        os.makedirs(os.path.join(extracted_data_path,folder), exist_ok=True)
+        df.to_csv(path)
+        logger.info("extracted data saved in, %s", path)
+
     except Exception as e:
         logger.error('Unexpected error, %s', e)
         raise
+
+def main():
+
+    try: 
+        df=load_data('https://raw.githubusercontent.com/aakashaldankar/Albany-Airbnb-Listings-Data/refs/heads/main/listings.csv')
+        save_data(df, folder='raw_data')
+
+    except Exception as e:
+        logger.error('Unexpected error, %e', e)
+        raise
+
+if __name__=='__main__':
+    main()
+
+
+
+        
 
 
 
