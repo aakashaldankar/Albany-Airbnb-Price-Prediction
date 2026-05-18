89916bada97ef8d92810707280d07d1261d8a954
aakashaldankar <haldankar.aakash.01@gmail.com>
2026-05-18 10:22:50 +0530

    created sample_dataframe in confest and implemented testing in test_data_ingestion.py

--- tests/unit/test_data_ingestion.py
+++ tests/unit/test_data_ingestion.py
@@ -1,4 +1,6 @@
-from src.data.data_ingestion import load_params
+from src.data.data_ingestion import load_params, load_data, pre_processing, save_data
+from confest import sample_dataframe
+import pandas as pd
 
 def test_load_params():
 
@@ -25,7 +27,38 @@ def test_load_params():
     assert isinstance(params['model_training']['hyper_parameters']['learning_rate'], float)
     assert isinstance(params['model_training']['hyper_parameters']['subsample'], float)
 
-# def test_pre_processing(sample_df):
+def test_load_data(tmp_path, sample_dataframe):
+
+    sample_file_path=tmp_path/'sample.csv'
+    sample_dataframe.to_csv(sample_file_path)
+
+    df=load_data(sample_file_path)
+
+    isinstance(df, pd.DataFrame)
+
+def test_pre_processing(sample_dataframe):
+
+    df=pre_processing(sample_dataframe)
+
+    assert df['price'].isnull().sum()==0
+
+    assert not {"id","listing_url",'scrape_id','last_scraped','source','picture_url','host_id',
+                 'host_url','host_thumbnail_url','host_picture_url','host_listings_count',
+                 'neighbourhood_group_cleansed','calendar_updated','calendar_last_scraped','license',
+                 'neighborhood_overview','host_about','host_neighbourhood','neighbourhood','estimated_occupancy_l365d','estimated_revenue_l365d'}.issubset(df.columns)
+    
+def test_save_data(tmp_path, sample_dataframe, monkeypatch):
+
+    monkeypatch.setattr('src.data.data_ingestion.data_path', str(tmp_path))
+    save_data(sample_dataframe, folder="raw_data", file_name="train")
+    saved_file = (tmp_path/"raw_data"/"train.csv")
+    assert saved_file.exists()
+
+
+
+
+    
+
 
 
 
